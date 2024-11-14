dc0952a04c857033dccde547580f90bc0931e590
Abhijit <kumarabhijeetpanda@rediffmail.com>
2024-11-14 09:36:02 +0530

    first commit and made all the componenets locally

---
+++ src/cms/data.js
@@ -0,0 +1,160 @@
+export const data = {
+  name: "Kumar Abhijit Panda",
+  role: "Software Engineer || Backend Enthusiast",
+  description:
+    "Building efficient and scalable backend systems with a focus on clean code and optimized performance.",
+  logo: "\\ Abhi. ",
+  about: `
+      Hello! I'm Kumar Abhijit Panda, a backend-focused developer with strong skills in Python, Django, and Django Rest Framework, complemented by full-stack experience. I specialize in building reliable and scalable backend systems, while also working across the stack to create cohesive and engaging web applications.\n
+      
+      With over a year of professional experience, I have developed RESTful APIs, designed efficient database schemas, and implemented business logic, ensuring performance and security. On the frontend, I enjoy crafting responsive interfaces and delivering seamless user experiences. My goal is to build end-to-end solutions that not only solve problems but also delight users with intuitive design and smooth functionality.
+      \n
+      When I'm not coding, I stay engaged with industry trends, contribute to projects, and focus on growing my skills in microservices, Docker, and Kubernetes.
+    `,
+  experiences: [
+    {
+      role: "Associate I",
+      company: "Capgemini Technology Services",
+      period: "September 2022 - March 2024",
+      responsibilities: [
+        "Developed and maintained scalable web applications using Python and Django, implementing REST APIs for seamless data exchange between client and server.",
+        "Utilized Docker to containerize applications, ensuring consistency across development, testing, and production environments.",
+        "Implemented Test-Driven Development (TDD) to write reliable and maintainable code, significantly reducing bugs and improving code quality",
+        "Managed continuous integration and deployment pipelines using GitHub Actions, automating testing and deployment processes to enhance efficiency and reliability.",
+        "Conducted regular code reviews and shared best practices in backend development",
+        "Led efforts to implement security measures, improving data protection and privacy",
+      ],
+    },
+  ],
+  skills: [
+    {
+      name: "Python",
+      // level: "Advanced",
+    },
+    {
+      name: "Java",
+      level: "Intermediate",
+    },
+    {
+      name: "Javascript",
+      level: "Intermediate",
+    },
+    {
+      name: "Django",
+      level: "Advanced",
+    },
+    {
+      name: "Django Rest Framework",
+      level: "Intermediate",
+    },
+    {
+      name: "Django Test Suite",
+      level: "Basic",
+    },
+    {
+      name: "Spring Boot",
+      level: "Basic",
+    },
+    {
+      name: "PostgrSQL",
+      level: "Intermediate",
+    },
+    {
+      name: "MySQL",
+      level: "Intermediate",
+    },
+    {
+      name: "Linux(Ubuntu)",
+      level: "Basic",
+    },
+    {
+      name: "Docker",
+      level: "Basic",
+    },
+    {
+      name: "Git",
+      level: "Basic",
+    },
+    {
+      name: "Github Actions",
+      level: "Basic",
+    },
+    {
+      name: "AWS",
+      level: "Basic",
+    },
+    {
+      name: "Microservices Architecture",
+      level: "Basic",
+    },
+    {
+      name: "ReactJs",
+      level: "Intermediate",
+    },
+    {
+      name: "HTML",
+      level: "Intermediate",
+    },
+    {
+      name: "CSS",
+      level: "Intermediate",
+    },
+    {
+      name: "TailwindCSS",
+      level: "Intermediate",
+    },
+    {
+      name: "Bootstrap",
+      level: "Intermediate",
+    },
+  ],
+  educations: [
+    {
+      courseName: "B.Tech, Computer Science and Engineering",
+      collegeName: "DRIEMS Autonomous Engineering College",
+      place: "Cuttack, Odisha",
+      period: "2018 - 2022",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1bKtMEn7yP7T1onMM9WGGg6TV5Y_-mxoE/view?usp=sharing",
+  photoUrl:
+    "https://drive.google.com/file/d/1RWkgj9zDdA6b2tFmz6ReX05nU5WvUUeA/view?usp=sharing",
+  projects: [
+    {
+      title: "Recipe API",
+      description:
+        "Developed a recipe API using Django Rest Framework with Test-Driven Development (TDD), Dockerized the app, implemented unit testing, and set up GitHub Actions for continuous integration and testing pipelines.",
+      technologies: [
+        "Django",
+        "Django Rest Framework",
+        "Django Test Suite",
+        "Docker",
+        "Github Actions",
+      ],
+      github: "https://github.com/Abhijit0303/recipe-app-api",
+      // live: "https://college-management-demo.yourdomain.com",
+    },
+    {
+      title: "Quiz Evaluation System",
+      description:
+        "Developed a command-line C program to process CSV files containing MCQ question papers and answer sheets, automate grading, and calculate total marks by comparing answers, showcasing strong file handling, data parsing, and problem-solving skills.",
+      technologies: ["C Lang", "CUnit", "CSV", "Excel"],
+      github: "https://github.com/Abhijit0303/Quiz_Evaluation_System",
+      // live: "https://college-management-demo.yourdomain.com",
+    },
+    {
+      title: "Portfolio Website",
+      description:
+        "Built a portfolio website using ReactJS, ShadCNUI, TailwindCSS, and Yarn, showcasing my skills with a modern, responsive design and efficient development workflow.",
+      technologies: ["ReactJs", "TailwindCSS", "ShaCN/UI", "Yarn"],
+      github: "https://github.com/yourusername/secure-api-gateway",
+      // live: "https://api.yourdomain.com",
+    },
+  ],
+  socialLinks: {
+    github: "https://github.com/Abhijit0303",
+    linkedin: "https://www.linkedin.com/in/kumar-abhijit-panda-a2b314200/",
+    twitter: "https://x.com/AbhijitCodex?t=_iQKrv0_rlpv-FKKd-xvVg&s=08 ",
+  },
+};
